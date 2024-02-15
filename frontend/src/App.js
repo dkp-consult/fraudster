33cf044c11f65d0ba8845ae30e0c403575deb3d6
dkp-consult <contact@dkp-consult.be>
2024-02-15 10:16:14 +0100

    dev : add wainting page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ConstructionPage from './pages/ConstructionPage';
 
 /* Testing connexion front 2 back */
 
 function App() {
-
-  const [backendMessage, setBackendMessage] = useState('');
-
-  useEffect(() => {
-    fetch('/api/test')
-      .then(response => response.json())
-      .then(data => setBackendMessage(data.message))
-      .catch(error => console.error("Erreur lors de la connexion au backend:", error));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {backendMessage ? backendMessage : "Chargement du message depuis le backend..."}
-      </header>
+    <div>
+      <ConstructionPage />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/ConstructionPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ConstructionPage = () => {
+  return (
+    
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center px-4">
+      <div className="text-center max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold text-gray-800">Projet en cours de construction</h1>
+        <p className="text-lg text-gray-600 mt-4">Notre site est actuellement en construction. Nous travaillons dur pour vous apporter la meilleure expérience possible. Restez à l'écoute !</p>
+      </div>
+    </div>
+  );
+};
+
+export default ConstructionPage;
